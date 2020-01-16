8983a5ed50156256f143fdc927a3396915f65540
James Swank <jmswank7@gmail.com>
2020-01-16 12:44:27 -0700

    prototype ready

--- app.py
+++ app.py
@@ -14,3 +14,47 @@ from navbar import Navbar
 df = pd.read_csv('https://raw.githubusercontent.com/jayohelee/dash-tutorial/master/data/population_il_cities.csv')
 df.set_index(df.iloc[:,0], drop = True, inplace = True)
 df = df.iloc[:,1:]
+
+nav = Navbar()
+
+header = html.H3(
+    'Select the name of an Illinois city to see its population!'
+)
+
+options = [{'label':x.replace(', Illinois', ''), 'value': x} for x in df.columns]
+
+dropdown = html.Div(dcc.Dropdown(
+    id = 'pop_dropdown',
+    options = options,
+    value = 'Abingdon city, Illinois'
+))
+
+output = html.Div(id = 'output',
+                children = [],
+                )
+
+def App():
+    layout = html.Div([
+        nav,
+        header,
+        dropdown,
+        output
+    ])
+    return layout
+
+def build_graph(city):
+  data = [go.Scatter(x = df.index,
+                          y = df[city],
+                          marker = {'color': 'orange'})]
+  graph = dcc.Graph(
+            figure = {
+                'data': data,
+  'layout': go.Layout(
+                      title = '{} Population Change'.format(city),
+                      yaxis = {'title': 'Population'},
+                      hovermode = 'closest'
+                                    )
+                        }
+              )
+  return graph
+
--- homepage.py
+++ homepage.py
@@ -16,8 +16,8 @@ body = dbc.Container(
                      html.H2("Heading"),
                      html.P(
                          """\
-Donec id elit non mi porta gravida at eget metus.Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumnibh, ut fermentum massa justo sit amet risus. Etiam porta semmalesuada magna mollis euismod. Donec sed odio dui. Donec id elit nonmi porta gravida at eget metus. Fusce dapibus, tellus ac cursuscommodo, tortor mauris condimentum nibh, ut fermentum massa justo sitamet risus. Etiam porta sem malesuada magna mollis euismod. Donec sedodio dui."""
-                           ),
+                      Donec id elit non mi porta gravida at eget metus.Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumnibh, ut fermentum massa justo sit amet risus. Etiam porta semmalesuada magna mollis euismod. Donec sed odio dui. Donec id elit nonmi porta gravida at eget metus. Fusce dapibus, tellus ac cursuscommodo, tortor mauris condimentum nibh, ut fermentum massa justo sitamet risus. Etiam porta sem malesuada magna mollis euismod. Donec sedodio dui."""
+                      ),
                            dbc.Button("View details", color="secondary"),
                    ],
                   md=4,
