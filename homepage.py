d1d91e7a93cd6b9695d5fbef876d426edf38a4d0
James Swank <jmswank7@gmail.com>
2020-02-12 19:06:26 -0700

    cleanup

--- homepage.py
+++ homepage.py
@@ -2,7 +2,6 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-# from navbar import Navbar
 
 body = dbc.Container([
    html.Div([
@@ -27,16 +26,8 @@ body = dbc.Container([
    ),
 ])
 
-# nav = Navbar()
-
 def Homepage():
     layout = html.Div([
-   #  nav,
     body
     ])
     return layout
-
-# app = dash.Dash(__name__, external_stylesheets = [dbc.themes.UNITED])
-# app.layout = Homepage()
-# if __name__ == "__main__":
-#     app.run_server()
